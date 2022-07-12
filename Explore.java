5a25008f36423a38a45a0399792dda3d79e76c27
awilliams217 <59180490+awilliams217@users.noreply.github.com>
2022-07-12 11:14:39 -0400

    Create Explore.java

---
+++ Explore.java
@@ -0,0 +1,10 @@
+
+import java.io.*;
+import java.util.*;
+//Breadth first search from vertex v.
+public interface Explore {
+    public void BFS(int i);
+boolean visited[] = new boolean[V];
+  
+    public void DFS(int i);
+}
